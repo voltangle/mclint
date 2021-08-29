1f0e291a5dfbfb007b3bab360556f89b3c71f1d7
ggoraa <ggoraa1029@gmail.com>
2021-08-29 22:21:15 +0300

    added PartialEq derive to MonkeyCStatement and Expression added var_type to VariableCreation enum

--- src/parser/syntax.rs
+++ src/parser/syntax.rs
@@ -1,18 +1,19 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum MonkeyCStatement {
     VariableCreation {
         name: String,
-        default_val: Expression
+        default_val: Expression,
+        var_type: Option<String>,
     },
     ClassDeclaration {
         name: String,
         extends: Option<String>,
-        children: Vec<MonkeyCStatement>
+        children: Vec<MonkeyCStatement>,
     }
     // Others will be added in later
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Expression {
     /// For "simple" assignment, like `var myVar = "simple string"`
     Simple(String)
