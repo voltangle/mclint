ce2802d3ed2a8ab69daa6d579f70b1735c9b89b5
ggoraa <ggoraa1029@gmail.com>
2021-08-29 22:23:35 +0300

    replaced unimplemented macros with self.currently_at incrementation

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -24,7 +24,6 @@ impl MonkeyCParser {
 
     pub fn parse(&mut self) -> Result<Vec<MonkeyCStatement>> {
         let mut statements: Vec<MonkeyCStatement> = Vec::new();
-        println!("{:?}", statements);
 
         // This is for understanding whether we finished to parse a line
         let mut line_finished = true;
@@ -32,97 +31,253 @@ impl MonkeyCParser {
             let t = self.current_token();
 
             match t.kind {
-                TokenKind::As => unimplemented!(),
-                TokenKind::And => unimplemented!(),
-                TokenKind::Break => unimplemented!(),
-                TokenKind::Case => unimplemented!(),
-                TokenKind::Catch => unimplemented!(),
-                TokenKind::Class => unimplemented!(),
-                TokenKind::Const => unimplemented!(),
-                TokenKind::Continue => unimplemented!(),
-                TokenKind::Default => unimplemented!(),
-                TokenKind::Do => unimplemented!(),
-                TokenKind::Else => unimplemented!(),
-                TokenKind::Enum => unimplemented!(),
-                TokenKind::Extends => unimplemented!(),
-                TokenKind::Finally => unimplemented!(),
-                TokenKind::For => unimplemented!(),
-                TokenKind::Function => unimplemented!(),
-                TokenKind::Has => unimplemented!(),
-                TokenKind::Hidden => unimplemented!(),
-                TokenKind::If => unimplemented!(),
-                TokenKind::InstanceOf => unimplemented!(),
-                TokenKind::Import => unimplemented!(),
-                TokenKind::Me => unimplemented!(),
-                TokenKind::Module => unimplemented!(),
-                TokenKind::Private => unimplemented!(),
-                TokenKind::Protected => unimplemented!(),
-                TokenKind::Public => unimplemented!(),
-                TokenKind::Or => unimplemented!(),
-                TokenKind::Return => unimplemented!(),
-                TokenKind::SelfK => unimplemented!(),
-                TokenKind::Static => unimplemented!(),
-                TokenKind::Switch => unimplemented!(),
-                TokenKind::Throw => unimplemented!(),
-                TokenKind::Try => unimplemented!(),
-                TokenKind::Using => unimplemented!(),
+                TokenKind::And => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Break => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Case => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Catch => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Class => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Const => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Continue => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Default => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Do => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Else => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Enum => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Extends => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Finally => {
+                    self.currently_at += 1;
+                },
+                TokenKind::For => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Function => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Has => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Hidden => {
+                    self.currently_at += 1;
+                },
+                TokenKind::If => {
+                    self.currently_at += 1;
+                },
+                TokenKind::InstanceOf => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Import => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Me => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Module => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Private => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Protected => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Public => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Or => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Return => {
+                    self.currently_at += 1;
+                },
+                TokenKind::SelfK => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Static => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Switch => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Throw => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Try => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Using => {
+                    self.currently_at += 1;
+                },
                 TokenKind::Var => {
+                    let mut name: String = String::new();
+                    let mut var_type: Option<String> = None;
+                    let mut default_val: Expression = Expression::Simple("".to_string());
+
                     self.currently_at += 1;
                     if !line_finished {
                         bail!(format!("Syntax error at {}:{}: Expected at least ';' token, found '{}'", self.current_token().row, self.current_token().column, self.current_token().literal));
                     }
                     line_finished = false;
-                    let statement = MonkeyCStatement::VariableCreation {
-                        name: self.current_token().literal.to_string(),
-                        default_val: {
+                    if self.current_token().kind != TokenKind::Identifier {
+                        bail!(format!("Syntax error at {}:{}: Expected an identifier, found '{}'", self.current_token().row, self.current_token().column, self.current_token().literal));
+                    }
+                    name = self.current_token().literal;
+                    self.currently_at += 1;
+                    match self.current_token().kind {
+                        TokenKind::Assign => {
                             self.currently_at += 1;
-                            if self.current_token().kind != TokenKind::Assign {
-                                bail!(format!("Syntax error at {}:{}: Expected '=' token, found '{}'", self.current_token().row, self.current_token().column, self.current_token().literal));
+                            let k = self.current_token().kind;
+                            if k == TokenKind::Identifier || k == TokenKind::BoolLiteral || k == TokenKind::CharLiteral || k == TokenKind::StringLiteral || k == TokenKind::CharLiteral || k == TokenKind::LongLiteral || k == TokenKind::DoubleLiteral || k == TokenKind::FloatLiteral || k == TokenKind::IntLiteral {} else {
+                                bail!(format!("Syntax error at {}:{}: Expected an identifier, found '{}'", self.current_token().row, self.current_token().column, self.current_token().literal));
                             }
-                            self.currently_at += 1;
-                            Expression::Simple(self.current_token().literal)
                         }
+                        TokenKind::As => {
+                            self.currently_at += 1;
+                            if self.current_token().kind != TokenKind::Identifier {
+                                bail!(format!("Syntax error at {}:{}: Expected an identifier after 'as', found '{}'", self.current_token().row, self.current_token().column, self.current_token().literal));
+                            }
+                            var_type = Some(self.current_token().literal);
+                            self.currently_at += 2;
+                            let k = self.current_token().kind;
+                            if k == TokenKind::Identifier || k == TokenKind::BoolLiteral || k == TokenKind::CharLiteral || k == TokenKind::StringLiteral || k == TokenKind::CharLiteral || k == TokenKind::LongLiteral || k == TokenKind::DoubleLiteral || k == TokenKind::FloatLiteral || k == TokenKind::IntLiteral {} else {
+                                bail!(format!("Syntax error at {}:{}: Expected an identifier or literal, found '{}'", self.current_token().row, self.current_token().column, self.current_token().literal));
+                            }
+                        }
+                        _ => {
+                            bail!(format!("Syntax error at {}:{}: Expected an '=' token or type declaration, found '{}'", self.current_token().row, self.current_token().column, self.current_token().literal));
+                        }
+                    }
+                    default_val = Expression::Simple(self.current_token().literal);
+                    let statement = MonkeyCStatement::VariableDeclaration {
+                        name,
+                        default_val,
+                        var_type
                     };
-                    println!("{:?}", statement);
                     statements.push(statement);
                     self.currently_at += 1;
                 }
-                TokenKind::While => unimplemented!(),
-                TokenKind::Bool => unimplemented!(),
-                TokenKind::String => unimplemented!(),
-                TokenKind::Int => unimplemented!(),
-                TokenKind::Long => unimplemented!(),
-                TokenKind::Float => unimplemented!(),
-                TokenKind::Double => unimplemented!(),
-                TokenKind::Null => unimplemented!(),
-                TokenKind::Char => unimplemented!(),
-                TokenKind::Nan => unimplemented!(),
-                TokenKind::New => unimplemented!(),
-                TokenKind::Identifier => unimplemented!(),
-                TokenKind::Comma => unimplemented!(),
-                TokenKind::OpeningBracket => unimplemented!(),
-                TokenKind::ClosingBracket => unimplemented!(),
-                TokenKind::OpeningBrace => unimplemented!(),
-                TokenKind::ClosingBrace => unimplemented!(),
-                TokenKind::Asterisk => unimplemented!(),
-                TokenKind::Percent => unimplemented!(),
-                TokenKind::Assign => unimplemented!(),
-                TokenKind::Bang => unimplemented!(),
-                TokenKind::Tilde => unimplemented!(),
-                TokenKind::Plus => unimplemented!(),
-                TokenKind::Minus => unimplemented!(),
-                TokenKind::Slash => unimplemented!(),
-                TokenKind::Ampersand => unimplemented!(),
-                TokenKind::LessThan => unimplemented!(),
-                TokenKind::GreaterThan => unimplemented!(),
-                TokenKind::Caret => unimplemented!(),
-                TokenKind::VerticalBar => unimplemented!(),
+                TokenKind::While => {
+                    self.currently_at += 1;
+                },
+                TokenKind::BoolLiteral => {
+                    self.currently_at += 1;
+                },
+                TokenKind::StringLiteral => {
+                    self.currently_at += 1;
+                },
+                TokenKind::IntLiteral => {
+                    self.currently_at += 1;
+                },
+                TokenKind::LongLiteral => {
+                    self.currently_at += 1;
+                },
+                TokenKind::FloatLiteral => {
+                    self.currently_at += 1;
+                },
+                TokenKind::DoubleLiteral => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Null => {
+                    self.currently_at += 1;
+                },
+                TokenKind::CharLiteral => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Nan => {
+                    self.currently_at += 1;
+                },
+                TokenKind::New => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Identifier => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Comma => {
+                    self.currently_at += 1;
+                },
+                TokenKind::OpeningBracket => {
+                    self.currently_at += 1;
+                },
+                TokenKind::ClosingBracket => {
+                    self.currently_at += 1;
+                },
+                TokenKind::OpeningBrace => {
+                    self.currently_at += 1;
+                },
+                TokenKind::ClosingBrace => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Asterisk => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Percent => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Assign => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Bang => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Tilde => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Plus => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Minus => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Slash => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Ampersand => {
+                    self.currently_at += 1;
+                },
+                TokenKind::LessThan => {
+                    self.currently_at += 1;
+                },
+                TokenKind::GreaterThan => {
+                    self.currently_at += 1;
+                },
+                TokenKind::Caret => {
+                    self.currently_at += 1;
+                },
+                TokenKind::VerticalBar => {
+                    self.currently_at += 1;
+                },
                 TokenKind::Semicolon => {
                     line_finished = true;
                     self.currently_at += 1;
                 }
+                _ => {
+                    self.currently_at += 1;
+                }
             }
         }
+        println!("{:?}", statements);
         Ok(statements)
     }
 }
