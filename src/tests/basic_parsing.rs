4326e557d268bc9e7cdfd4ae6b5f5c515cdf37ce
ggoraa <ggoraa1029@gmail.com>
2021-09-01 19:38:00 +0300

    fixed test

--- src/tests/basic_parsing.rs
+++ src/tests/basic_parsing.rs
@@ -3,17 +3,18 @@ use crate::parser::MonkeyCParser;
 use anyhow::Context;
 use anyhow::Result;
 use crate::parser::syntax::MonkeyCStatement::VariableDeclaration;
-use crate::parser::syntax::MonkeyCExpression::Simple;
+use crate::parser::syntax::MonkeyCExpression;
 
 #[test]
 fn basic_code() -> Result<()> {
     let data = "var myVar as String = \"Hm\";\nvar myOtherVar = myVar;";
     let mut lexer = MonkeyCLexer::new(data.chars().collect());
     let tokens = lexer.lex().with_context(|| "Failed to tokenize data")?;
+    println!("{:?}", tokens.clone());
     let mut parser = MonkeyCParser::new(tokens);
 
     // Check for equality of parsed syntax
-    assert_eq!(parser.parse().with_context(|| "Failed to parse data")?, vec![VariableDeclaration { name: "myVar".to_string(), default_val: Simple("Hm".to_string()), var_type: Some("String".to_string()), is_const: false },
-                                                                             VariableDeclaration { name: "myOtherVar".to_string(), default_val: Simple("myVar".to_string()), var_type: None, is_const: false }]);
+    assert_eq!(parser.parse().with_context(|| "Failed to parse data")?, vec![VariableDeclaration { name: "myVar".to_string(), default_val: MonkeyCExpression::Simple("Hm".to_string()), var_type: Some("String".to_string()), is_const: false },
+                                                                             VariableDeclaration { name: "myOtherVar".to_string(), default_val: MonkeyCExpression::Reference("myVar".to_string()), var_type: None, is_const: false }]);
     Ok(())
 }
