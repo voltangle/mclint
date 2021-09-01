5a5b8882f3604f13bef5716d30cd9562ac8b8bff
ggoraa <ggoraa1029@gmail.com>
2021-09-01 19:25:43 +0300

    added new expression types

--- src/parser/syntax.rs
+++ src/parser/syntax.rs
@@ -2,8 +2,9 @@
 pub enum MonkeyCStatement {
     VariableDeclaration {
         name: String,
-        default_val: Expression,
+        default_val: MonkeyCExpression,
         var_type: Option<String>,
+        is_const: bool
     },
     ClassDeclaration {
         name: String,
@@ -14,8 +15,71 @@ pub enum MonkeyCStatement {
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Expression {
-    /// For "simple" assignment, like `var myVar = "simple string"`
-    Simple(String)
+pub enum MonkeyCExpression {
+    /// "Simple" assignment
+    /// # Example
+    /// ```
+    /// var myVar = "simple string"
+    /// ```
+    Simple(String),
+    /// Reference to other variables/functions/classes/etc
+    ///
+    /// # Examples
+    /// ```
+    /// var fooVar = fooFunc();
+    /// var barVar = new BarClass();
+    /// var bazVar = otherBazVar;
+    /// ```
+    Reference(String),
+    /// Mathematical equations.
+    /// # Examples
+    /// ```
+    /// var fooVar = 1 + 2;
+    /// var barVar = fooVar * 2;
+    /// var bazVar = barVar / 2;
+    /// ```
+    Mathematical(Box<MonkeyCExpression>, MonkeyCExprMathOperation, Box<MonkeyCExpression>),
+    /// Binary operations.
+    /// # Examples
+    /// ```
+    /// var fooVar = 1 <= 2;
+    /// if (fooVar) { // Should be true, otherwise the language broke
+    ///     // Do smth
+    /// }
+    /// ```
+    Binary(Box<MonkeyCExpression>, MonkeyCExprBinaryOperation, Box<MonkeyCExpression>),
+    /// Bitwise operations.
+    /// # Examples
+    /// ```
+    /// var fooVar = 0x111 << 0x001;
+    /// ```
+    Bitwise(Box<MonkeyCExpression>, MonkeyCExprBitwiseOperation, Box<MonkeyCExpression>)
+}
 
+#[derive(Debug, PartialEq)]
+pub enum MonkeyCExprBitwiseOperation {
+    LeftShift,
+    RightShift,
+    And,
+    Or,
+    Xor
+}
+
+#[derive(Debug, PartialEq)]
+pub enum MonkeyCExprBinaryOperation {
+    LessThan,
+    LessThanEquals,
+    GreaterThan,
+    GreaterThanEquals,
+    Equals,
+    NotEquals
+}
+
+#[derive(Debug, PartialEq)]
+pub enum MonkeyCExprMathOperation {
+    Add,
+    Subtract,
+    Divide,
+    Multiply,
+    Modulo,
 }
