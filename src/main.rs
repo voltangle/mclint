e567c35d3c2cc7ccaaaeb9d6402d76a1b4ba5cbe
ggoraa <ggoraa1029@gmail.com>
2021-09-03 07:40:48 +0300

    WIP: Reworking error handling

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use clap::{App, Arg};
 use std::fs;
 use std::path::PathBuf;
 use crate::parser::MonkeyCParser;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::slice::range;
 
 fn main() -> Result<()> {
     let matches = App::new("MCLint")
@@ -29,11 +32,51 @@ fn main() -> Result<()> {
         .with_context(|| format!("Failed to read contents of {}", file_path.display()))?;
 
     let mut lexer = MonkeyCLexer::new(file_contents.chars().collect());
-    let tokens = lexer.lex().with_context(|| format!("Failed to tokenize {:?}", file_path))?;
+    let tokens = lexer.lex().with_context(|| format!("Failed to tokenize {:?}", file_path.clone()))?;
 
     let mut parser = MonkeyCParser::new(tokens);
-    parser.parse().with_context(|| format!("Failed to parse {:?}", file_path))?;
+    match parser.parse() {
+        Ok(data) => {
+            println!("{:?}", data);
+        }
+        Err(errors) => {
+            for error in errors {
+                let mut error_str = String::new();
+
+                // Getting a line specified inside of error struct
+                let file = File::open(file_path.clone()).unwrap();
+                let reader = BufReader::new(file);
+                for (index, line) in reader.lines().enumerate() {
+                    if index + 1 == error.at.0 as usize {
+                        error_str.push_str(&*line.unwrap());
+                        break;
+                    }
+                }
+                error_str.push('\n');
+
+                // Adding an underline
+                for _ in 0..error.at.1 {
+                    error_str.push(' ');
+                }
+                for _ in 0..error.literal_len {
+                    error_str.push('~');
+                }
+                error_str.push('\n');
+
+                // Adding a small pointer
+                for _ in 0..error.at.1 {
+                    error_str.push(' ');
+                }
+                error_str.push_str("|\n");
+
+                for _ in 0..error.at.1 {
+                    error_str.push(' ');
+                }
+            }
+        }
+    }
 
     println!("{:?}", matches.value_of("INPUT"));
     Ok(())
 }
+
